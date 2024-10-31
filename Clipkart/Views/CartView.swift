0ea05df217f651646512c61cf5d89b3a9871958f
pratikrn <pnpatil7609@gmail.com>
2024-10-31 15:58:54 +0530

    changes during development phase 1

---
+++ Clipkart/Views/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  Clipkart
+//
+//  Created by pratik.nalawade on 31/10/24.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
